20b9d2c044adb7bd6fbddab104c14cf1fef0bd16
cmeloni <carlo.meloni@abinsula.com>
2023-12-27 16:34:43 +0100

    feat: rendering form errors

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,41 +1,77 @@
-"use client"
+"use client";
 
 import React from "react";
 import AuthFormContainer from "@components/AuthFormContainer";
 import { Button, Input } from "@material-tailwind/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { useFormik } from "formik";
-import * as yup from 'yup';
+import * as yup from "yup";
+import { filterFormikErrors } from "@/app/utils/formikHelpers";
 
 const validationSchema = yup.object().shape({
   name: yup.string().required("Un nome è richiesto."),
-  email: yup.string().email("Inserisci un'email valida.").required("Un'email' è richiesta."),
-  password: yup.string().min(8, "La password deve avere minimo 8 caratteri.").required("Una password è richiesta."),
-})
+  email: yup
+    .string()
+    .email("Inserisci un'email valida.")
+    .required("Un'email è richiesta."),
+  password: yup
+    .string()
+    .min(8, "La password deve avere minimo 8 caratteri.")
+    .required("Una password è richiesta."),
+});
 
 export default function SignUp() {
-  const {values, handleBlur, handleChange, handleSubmit, isSubmitting, errors, touched} = useFormik({
-      initialValues: {
-        name: '',
-        email: '',
-        password: ''
-      },
-      validationSchema,
-      onSubmit: (values) => {
-        console.log('values', values)
-      }
-    }
-  )
-  const formErrors: string[] = [];
+  const {
+    values,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    isSubmitting,
+    errors,
+    touched,
+  } = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log("values", values);
+    },
+  });
 
-  console.log('errosrs', errors)
-  const {name, email, password} = values
+  const formErrors: string[] = filterFormikErrors(errors, touched, values);
+
+  const { name, email, password } = values;
 
   return (
     <AuthFormContainer title="Create New Account" onSubmit={handleSubmit}>
-      <Input name="name" label="Name" onChange={handleChange} value={name} crossOrigin={undefined} />
-      <Input name="email" label="Email" onChange={handleChange} value={email} crossOrigin={undefined} />
-      <Input name="password" label="Password" type="password" onChange={handleChange} value={password} crossOrigin={undefined} />
+      <Input
+        name="name"
+        label="Name"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={name}
+        crossOrigin={undefined}
+      />
+      <Input
+        name="email"
+        label="Email"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={email}
+        crossOrigin={undefined}
+      />
+      <Input
+        name="password"
+        label="Password"
+        type="password"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={password}
+        crossOrigin={undefined}
+      />
       <Button type="submit" className="w-full">
         Sign up
       </Button>
