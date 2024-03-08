297cd166bed130773976813ef5697b1c58fac04a
cmelonijs <carlomeloni63@gmail.com>
2024-03-08 16:00:58 +0100

    feat: hashing password

--- app/models/userModel.ts
+++ app/models/userModel.ts
@@ -1,3 +1,4 @@
+import { genSalt, hash } from 'bcrypt';
 import { Document, Model, Schema, model, models } from 'mongoose';
 
 interface UserDocument extends Document {
@@ -17,6 +18,17 @@ const UserSchema = new Schema<UserDocument>({
     avatar: {type: Object, url: String, id: String},
     verified: {type: Boolean, default: false}
 }, {timestamps: true})
+
+UserSchema.pre("save", async function (next) {
+    try {
+        if(!this.isModified("password")) return next();
+        const salt = await genSalt(12)
+        this.password = await hash(this.password, salt);
+        next()
+    } catch (err) {
+        console.log('err', err)
+    }
+})
 
 const UserModel = models.User || model('User', UserSchema);
 
